4e18661c9d7c8fca209b850d0d048f4d3df9c6de
maksimyakush <maksimo-1988@yandex.ru>
2019-01-17 17:31:24 +0300

    feat(task-1):add commented code to implement

--- sources/models/countries.js
+++ sources/models/countries.js
@@ -8,3 +8,33 @@ export const countries = new webix.DataCollection({
 		{ "Short Name": "EN", "Full Name": "England" }
 	]
 });
+
+// { id:"data:countries", $subview : new CommonData(this.app,"", countries, ["name", "icon"])},
+
+// constructor(app,name,data, columns){
+// 		super(app,name);
+// 		this._tdata = data;
+//         this._columns = colunbs
+// 	}
+//     config(){
+//         let cols = [];
+//         columns.foreach((name, i){
+//             cols.push({
+//                 id:i,
+//                 header:i,
+//                 fillspace:!i,
+//             })
+//         })
+//         cols.push({
+//             template:"{common.trashIcon()}"
+//         });
+
+//         return {
+//             view:"datatabke", column:cols, onClick:{
+//                 "wxi-trash":fucntion(){
+//                     this.remove(id)
+//                 }
+//             }
+//         }
+
+//     }
